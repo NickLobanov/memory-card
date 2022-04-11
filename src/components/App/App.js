236ab694e76c2c78919f5fb12a16f840d926e9ba
Николай Лобанов <nick@192.168.0.3>
2022-04-11 21:11:42 +0300

    first commit

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import {Routes, Route} from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+      hello
+    </div>
+  );
+}
+
+export default App;
